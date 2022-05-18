561d02982d5425251f97f31c7519539fa63dcd9f
NekoCarreraDesigns <denvermaas@gmail.com>
2022-05-18 12:47:17 -0600

    added middleware to server.js

--- youve-never-heard-them/server.js
+++ youve-never-heard-them/server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.listen(port, function () {
   console.log("Server listening at: http://localhost:" + port);
 });
