2bc9b087c21a90cc5676616f6afa569074d59f3e
NekoCarreraDesigns <denvermaas@gmail.com>
2022-05-23 13:54:33 -0600

    added vinyl icon, to header and public folder

--- youve-never-heard-them/src/components/header/index.js
+++ youve-never-heard-them/src/components/header/index.js
@@ -4,7 +4,11 @@ import "./styles.css";
 function Header() {
   return (
     <div>
-      <h1 className='page-header'>You've Never Heard of Them</h1>
+      <h1 className='page-header'>
+        <img alt='vinyl-record' className='app-logo' src='vinyl.png'></img>{" "}
+        You've Never Heard of Them{" "}
+        <img alt='vinyl-record' className='app-logo' src='vinyl.png'></img>
+      </h1>
       <h2 className='second-page-header'>Music blog for music snobs</h2>
     </div>
   );
