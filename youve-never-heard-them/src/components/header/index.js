5f0fbdd001b364c08585271b2e06bf24f2dede4b
NekoCarreraDesigns <denvermaas@gmail.com>
2022-05-23 13:34:06 -0600

    added page header

---
+++ youve-never-heard-them/src/components/header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+function Header() {
+  return (
+    <div>
+      <h1 className='page-header'>You've Never Heard of Them</h1>
+      <h2 className='second-page-header'>Music blog for music snobs</h2>
+    </div>
+  );
+}
+
+export default Header;
