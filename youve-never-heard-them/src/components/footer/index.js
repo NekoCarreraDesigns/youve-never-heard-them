9ca351f3766b2d80176e5c45d708425d1a6c6afb
NekoCarreraDesigns <denvermaas@gmail.com>
2022-05-23 13:45:49 -0600

    added link to footer for icon attribution

---
+++ youve-never-heard-them/src/components/footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div className='page-footer'>
+        <small>Developed by Neko Carrera Designs &copy; 2022</small>
+        <a href='https://www.flaticon.com/free-icons/vinyl' title='vinyl icons'>
+          Vinyl icons created by Those Icons - Flaticon
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
