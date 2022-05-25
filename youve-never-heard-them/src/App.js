97bbb8e8b4ea5d730c27daeb32b9cbde3e0cc9c8
NekoCarreraDesigns <denvermaas@gmail.com>
2022-05-25 14:56:33 -0600

    added navbar to App.js

--- youve-never-heard-them/src/App.js
+++ youve-never-heard-them/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/header";
+import Navbar from "./components/navbar";
 import "./App.css";
 
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <Header />
     </div>
   );
