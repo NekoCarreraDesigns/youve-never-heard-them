c3c3d7d07f6b7da8a0d0ac1b970db659d8f208bf
NekoCarreraDesigns <denvermaas@gmail.com>
2022-05-26 10:07:05 -0600

    added music icon to navbar

--- youve-never-heard-them/src/components/navbar/index.js
+++ youve-never-heard-them/src/components/navbar/index.js
@@ -8,7 +8,10 @@ function Navbar() {
         <img alt='record-crate' src='crate.png'></img>
       </a>
       <a className='navbar-link' href='/forum'>
-        <img alt='record-crate' src='chat.png'></img>
+        <img alt='thought-bubble' src='chat.png'></img>
+      </a>
+      <a className='navbar-link' href='/music'>
+        <img alt='vinyl-player' src='vinyl-player.png'></img>
       </a>
     </div>
   );
