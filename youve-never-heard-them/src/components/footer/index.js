7fede6f9ba1a75cbcd893dbcd95171cc613c6ba4
NekoCarreraDesigns <denvermaas@gmail.com>
2022-05-27 10:43:17 -0600

    added line breaks to attribution links

--- youve-never-heard-them/src/components/footer/index.js
+++ youve-never-heard-them/src/components/footer/index.js
@@ -6,17 +6,21 @@ function Footer() {
     <footer>
       <div className='page-footer'>
         <small>Developed by Neko Carrera Designs &copy; 2022</small>
+        <br />
         <a href='https://www.flaticon.com/free-icons/vinyl' title='vinyl icons'>
           Vinyl icons created by Those Icons - Flaticon
         </a>
+        <br />
         <a href='https://www.flaticon.com/free-icons/crate' title='crate icons'>
           Crate icons created by Smashicons - Flaticon
         </a>
+        <br />
         <a
           href='https://www.flaticon.com/free-icons/message'
           title='message icons'>
           Message icons created by Becris - Flaticon
         </a>
+        <br />
         <a
           href='https://www.flaticon.com/free-icons/dj-music'
           title='dj music icons'>
