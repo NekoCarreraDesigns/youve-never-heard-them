8cdb9a00e3c245005f47bd9d02d897429e909781
NekoCarreraDesigns <denvermaas@gmail.com>
2022-05-30 13:58:39 -0600

    added header to main section

---
+++ youve-never-heard-them/src/components/main/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+function Main() {
+  return (
+    <div>
+      <h1 className='main-header'>Cave In</h1>
+    </div>
+  );
+}
+
+export default Main;
