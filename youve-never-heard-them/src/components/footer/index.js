3d3e73700bb83d7c22f81d2de3feaafef5c9d68f
NekoCarreraDesigns <denvermaas@gmail.com>
2022-05-30 13:29:19 -0600

    added classes to links

--- youve-never-heard-them/src/components/footer/index.js
+++ youve-never-heard-them/src/components/footer/index.js
@@ -7,21 +7,29 @@ function Footer() {
       <div className='page-footer'>
         <small>Developed by Neko Carrera Designs &copy; 2022</small>
         <br />
-        <a href='https://www.flaticon.com/free-icons/vinyl' title='vinyl icons'>
+        <a
+          className='footer-link'
+          href='https://www.flaticon.com/free-icons/vinyl'
+          title='vinyl icons'>
           Vinyl icons created by Those Icons - Flaticon
         </a>
         <br />
-        <a href='https://www.flaticon.com/free-icons/crate' title='crate icons'>
+        <a
+          className='footer-link'
+          href='https://www.flaticon.com/free-icons/crate'
+          title='crate icons'>
           Crate icons created by Smashicons - Flaticon
         </a>
         <br />
         <a
+          className='footer-link'
           href='https://www.flaticon.com/free-icons/message'
           title='message icons'>
           Message icons created by Becris - Flaticon
         </a>
         <br />
         <a
+          className='footer-link'
           href='https://www.flaticon.com/free-icons/dj-music'
           title='dj music icons'>
           Dj music icons created by Bima Pamungkas - Flaticon
