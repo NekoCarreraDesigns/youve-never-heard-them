e85116825264e1a00db795890e58829791aa8205
NekoCarreraDesigns <denvermaas@gmail.com>
2022-06-01 10:56:25 -0600

    added rotation section to main

--- youve-never-heard-them/src/components/main/index.js
+++ youve-never-heard-them/src/components/main/index.js
@@ -20,13 +20,32 @@ function Main() {
       <img
         className='main-image'
         alt='band'
-        width={400}
-        height={300}
+        width={500}
+        height={400}
         src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa_k3GhP7gEfb0TYMZYF5PBS7qjgeHqeM48A&usqp=CAU'></img>
       <p className='main-paragraph'>
         Texas funky soul chill out music that should be enjoyed under the stars,
         with a lover snuggled up under a blanket
       </p>
+      <div className='rotation-section'>
+        <h1 className='main-rotation-header'>Current Listening Rotation</h1>
+        <div className='rotation-artist'>
+          <h2 className='rotation-header'>Church of Misery</h2>
+          <img
+            alt='band'
+            height={400}
+            width={500}
+            src='https://riseaboverecords.com/wp-content/uploads/Church-of-Misery-2013.jpg'></img>
+        </div>
+        <div className='rotation-artist'>
+          <h2 className='rotation-header'>Church of Misery</h2>
+          <img
+            alt='band'
+            height={400}
+            width={500}
+            src='https://riseaboverecords.com/wp-content/uploads/Church-of-Misery-2013.jpg'></img>
+        </div>
+      </div>
     </div>
   );
 }
