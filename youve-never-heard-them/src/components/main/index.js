4217fa28cd2ff44b90c917bfc08f8149dd126ab8
NekoCarreraDesigns <denvermaas@gmail.com>
2022-06-01 10:32:44 -0600

    added second section to main

--- youve-never-heard-them/src/components/main/index.js
+++ youve-never-heard-them/src/components/main/index.js
@@ -16,6 +16,17 @@ function Main() {
         combining all the elements of previous material into a hardcore space
         rock heavy metal ball of thunder
       </p>
+      <h1 className='main-header'>Khruangbin</h1>
+      <img
+        className='main-image'
+        alt='band'
+        width={400}
+        height={300}
+        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa_k3GhP7gEfb0TYMZYF5PBS7qjgeHqeM48A&usqp=CAU'></img>
+      <p>
+        Texas funky soul chill out music that should be enjoyed under the stars,
+        with a lover snuggled up under a blanket
+      </p>
     </div>
   );
 }
