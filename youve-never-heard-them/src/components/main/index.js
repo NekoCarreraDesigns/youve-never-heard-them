12d6b9f1cd2751e7e14e011e27d0c06df0206b24
NekoCarreraDesigns <denvermaas@gmail.com>
2022-06-02 11:55:03 -0600

    added fifth and sixth section

--- youve-never-heard-them/src/components/main/index.js
+++ youve-never-heard-them/src/components/main/index.js
@@ -27,30 +27,30 @@ function Main() {
         Texas funky soul chill out music that should be enjoyed under the stars,
         with a lover snuggled up under a blanket
       </p>
+      <h1 className='main-rotation-header'>Current Listening Rotation</h1>
       <div className='rotation-section'>
-        <h1 className='main-rotation-header'>Current Listening Rotation</h1>
         <div className='rotation-artist'>
           <h2 className='rotation-header'>Church of Misery</h2>
           <img
             alt='band'
-            height={200}
-            width={200}
+            height={500}
+            width={400}
             src='https://riseaboverecords.com/wp-content/uploads/Church-of-Misery-2013.jpg'></img>
         </div>
         <div className='rotation-artist'>
           <h2 className='rotation-header'>Khruangbin</h2>
           <img
             alt='band'
-            height={200}
-            width={200}
+            height={500}
+            width={400}
             src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1eZVcMkFOyhgPPvy18jkAOB4GS4dWW_aWkQ&usqp=CAU'></img>
         </div>
         <div className='rotation-artist'>
           <h2 className='rotation-header'>An Autumn for Crippled Children</h2>
           <img
             alt='band'
-            height={200}
-            width={200}
+            height={500}
+            width={400}
             src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLM9p3X8xSwflUQNAWjdB7UF1tp2ACBOFiTw&usqp=CAU'></img>
         </div>
       </div>
@@ -59,9 +59,25 @@ function Main() {
           <h2 className='rotation-header'>1000mods</h2>
           <img
             alt='band'
-            width={200}
-            height={200}
+            width={400}
+            height={500}
             src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxBOjCYUb7YJM6KiORQvT8Pc7Ix1Au75vBGA&usqp=CAU'></img>
+        </div>
+        <div className='rotation-artist'>
+          <h2 className='rotation-header'>Still Corners</h2>
+          <img
+            alt='band'
+            width={400}
+            height={500}
+            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcOMghhRffRg9u5mOEuJTPGFmpZjSgoVke_Q&usqp=CAU'></img>
+        </div>
+        <div className='rotation-artist'>
+          <h2 className='rotation-header'>Jupiterian</h2>
+          <img
+            alt='band'
+            width={400}
+            height={500}
+            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXUVaXWQBAjQJJIGjWDPOoKCh48VJTB8xLsQ&usqp=CAU'></img>
         </div>
       </div>
     </div>
